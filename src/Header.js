5428aacd30024887c71ce367fd4d55e4e43f4801
Curtis Ung <curtisung4@gmail.com>
2024-01-31 03:17:31 -0800

    added header image and alignment

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import "./Header.css";
 import MenuIcon from '@mui/icons-material/Menu';
+import { IconButton } from "@mui/material";
 
 function Header() {
     return (
         <div className="header">
             <div className="header__left">
-                <MenuIcon/>
+                <IconButton>
+                    <MenuIcon/>
+                </IconButton>
+                <img src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png" alt="gmail"/>
             </div>
             <div className="header__middle"></div>
             <div classname="header__right"></div>
