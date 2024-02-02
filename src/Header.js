7caa9f91b64887d9f45af7095057e831ea9ac8e1
curtisung <45185308+curtisung@users.noreply.github.com>
2024-02-02 00:43:11 +0000

    added search bar header formatting, video timestamp 38:11

--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React from "react";
 import "./Header.css";
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from "@mui/icons-material/Menu";
 import { IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 function Header() {
-    return (
-        <div className="header">
-            <div className="header__left">
-                <IconButton>
-                    <MenuIcon/>
-                </IconButton>
-                <img src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png" alt="gmail"/>
-            </div>
-            <div className="header__middle"></div>
-            <div classname="header__right"></div>
-        </div>
-    );
+  let searchPlaceholderText = "Search mail";
+  return (
+    <div className="header">
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png"
+          alt="gmail"
+        />
+      </div>
+      <div className="header__middle">
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+        <input placeholder={searchPlaceholderText}></input>
+        <ArrowDropDownIcon className="header__inputCaret" />
+      </div>
+      <div classname="header__right"></div>
+    </div>
+  );
 }
 
 export default Header;
