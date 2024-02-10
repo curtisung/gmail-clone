5bdab2350498ef04a5505fc5fe83a715ca34cece
curtisung <45185308+curtisung@users.noreply.github.com>
2024-02-10 00:53:48 +0000

    added color and darkening to tabs on hover

--- src/MailTab.js
+++ src/MailTab.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+
 import "./MailTab.css";
 
-export default function MailTab({ Icon, title, color, isSelected}) {
-    return (
-        <div className="mailTab" style={{
-            borderBottom: `3px solid ${color}`,
-            color: `${isSelected && color}`
-        }}>
-            <div className="mailTab__icon">
-                <Icon/>
-            </div>
-            <h4>{title}</h4>
-        </div>
-    );
+export default function MailTab({ Icon, title, color, isSelected }) {
+  const [isActive, setIsActive] = useState(isSelected);
+
+  return (
+    <div
+      className={`mailTab ${isSelected && "mailTab--active"}`}
+      style={{
+        borderBottomColor: `${isActive && color}`,
+        color: isActive || isSelected ? color : null,
+      }}
+      onMouseEnter={() => setIsActive(true)}
+      onMouseLeave={() => setIsActive(false)}
+    >
+      <div className="mailTab__icon">
+        <Icon />
+      </div>
+      <h4>{title}</h4>
+    </div>
+  );
 }
